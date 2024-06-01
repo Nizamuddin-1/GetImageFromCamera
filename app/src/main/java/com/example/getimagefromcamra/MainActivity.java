d52bc0618abc52ea30e163fd011ada13b4cf4eb4
Nizamuddin-1 <Nizamuddin9917365029@gmail.com>
2024-06-01 16:33:11 +0530

    Add file

---
+++ app/src/main/java/com/example/getimagefromcamra/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.getimagefromcamra;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private  final int CAMERA_REQ_CODE=100;
+ImageView imageView;
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.camera);
+        imageView=findViewById(R.id.image);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,CAMERA_REQ_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            if(requestCode==CAMERA_REQ_CODE)
+            {
+                Bitmap img=(Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(img);
+            }
+        }
+    }
+}
